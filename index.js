bc84a88eff92f51f931d40027b3471296d5a67d5
zacharie naim <melvin.naim@gmail.com>
2025-07-16 19:56:58 +0300

    update back

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-const express=require ('express');
-const http = require('http');
-const cors = require('cors');
-const { Server } = require('socket.io');
+const express = require("express");
+const http = require("http");
+const cors = require("cors");
+const { Server } = require("socket.io");
 
 const app = express();
 app.use(cors());
@@ -9,24 +9,40 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: '*',
-    methods: ['GET', 'POST'],
+    origin: "https://react-chat-eta-seven.vercel.app/",
+    methods: ["GET", "POST"],
   },
 });
 
-io.on('connection', (socket) => {
-    console.log('A user connected:', socket.id);
+const users = {};
 
-    socket.on('send_message', (message) => {
-        console.log('Message received:', message);
-        io.emit('receive_message', message);
-    });
+io.on("connection", (socket) => {
+  console.log("User connected", socket.id);
 
-    socket.on('disconnect', () => {
-        console.log('User disconnected:', socket.id);
+  socket.on("join-user", (username) => {
+    users[socket.id] = username;
+    console.log(`${username} connected`);
+  });
+
+  socket.on("join-private-room", ({ room }) => {
+    socket.join(room);
+    console.log(`${username} join the room ${room}`);
+  });
+
+  socket.on("private-message", ({ room, message }) => {
+    const username = users[socket.id];
+    console.log(`Message from ${username} in ${room}: ${message}`);
+
+    io.to(room).emit("receive_private_message", {
+      message,
+      username,
     });
+  });
+
+  socket.on("disconnect", () => {
+    delete users[socket.id];
+  });
 });
 server.listen(3001, () => {
-    console.log('Chat server is running on port 3001');
+  console.log("Chat server is running on port 3001");
 });
-
